c8950222da6f502a9148f30b2475bf24028ad586
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-06-23 09:53:30 -0400

    Initial Commit

---
+++ galleryScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  galleryScroll
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-06-23.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
